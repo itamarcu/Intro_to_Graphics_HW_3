b78cea4be0a43b65c85be85215d2b32324723849
itamar <itamar@feezback.com>
2018-06-05 12:36:04 +0300

    Fixed image rotation and SS thing. Spheres image is now working correctly.

--- src/RayTracer.java
+++ src/RayTracer.java
@@ -7,121 +7,104 @@ import java.io.*;
 /**
  * Main class for ray tracing exercise.
  */
-public class RayTracer
-{
+public class RayTracer {
     private int imageWidth;
     private int imageHeight;
     private Scene scene;
     private Camera camera;
-    
+
     /**
      * Runs the ray tracer. Takes scene file, output image file and optional image size as input.
      */
-    public static void main(String[] args)
-    {
-        
-        try
-        {
-            
+    public static void main(String[] args) {
+
+        try {
+
             RayTracer tracer = new RayTracer();
-            
+
             // Default values:
             tracer.imageWidth = 500;
             tracer.imageHeight = 500;
-            
+
             if (args.length < 2)
                 throw new RayTracerException("Not enough arguments provided. Please specify an input scene file and " +
                         "an output image file for rendering.");
-            
+
             String sceneFileName = args[0];
             String outputFileName = args[1];
-            
-            if (args.length > 3)
-            {
+
+            if (args.length > 3) {
                 tracer.imageWidth = Integer.parseInt(args[2]);
                 tracer.imageHeight = Integer.parseInt(args[3]);
             }
-            
-            
+
+
             tracer.parseScene(sceneFileName);
-            
+
             tracer.renderScene(outputFileName);
-        }
-        catch (RayTracerException e)
-        {
+        } catch (RayTracerException e) {
             e.printStackTrace();
         }
-        
-        
+
+
     }
-    
-    private class Parser
-    {
+
+    private class Parser {
         private String[] params;
         private int paramsIndex;
-        
-        public Parser(String[] params)
-        {
+
+        public Parser(String[] params) {
             this.params = params;
             this.paramsIndex = 0;
         }
-        
-        // See above comments
-        private int aInt()
-        {
+
+        private int aInt() {
             return Integer.parseInt(params[paramsIndex++]);
         }
-        
-        // See above comments
-        private double aDouble()
-        {
+
+        private double aDouble() {
             return Double.parseDouble(params[paramsIndex++]);
         }
-        
-        // See above comments
-        private Vec3 aVec3()
-        {
+
+        private Vec3 aVec3() {
             return new Vec3(aDouble(), aDouble(), aDouble());
         }
-        
-        // See above comments
-        private Color aColor()
-        {
+
+        private Color aColor() {
             return new Color(aDouble(), aDouble(), aDouble());
         }
+
+        private boolean notDone() {
+            return paramsIndex < params.length;
+        }
     }
-    
+
     /**
      * Parses the scene file and creates the scene. Change this function so it generates the required objects.
      */
-    private void parseScene(String sceneFileName) throws RayTracerException
-    {
+    private void parseScene(String sceneFileName) throws RayTracerException {
         scene = new Scene();
-        
-        try
-        {
+
+        try {
             FileReader fr = new FileReader(sceneFileName);
             BufferedReader r = new BufferedReader(fr);
             System.out.println("Started parsing scene file " + sceneFileName);
-            
+
             String line;
             int lineNum = 0;
-            while ((line = r.readLine()) != null)
-            {
+            while ((line = r.readLine()) != null) {
                 line = line.trim();
                 ++lineNum;
-                
-                if (line.isEmpty() || (line.charAt(0) == '#'))
-                {  // This line in the scene file is a comment
+
+                if (line.isEmpty() || (line.charAt(0) == '#')) {  // This line in the scene file is a comment
                     continue;
                 }
                 String code = line.substring(0, 3).toLowerCase();
                 // Split according to white space characters:
                 String[] params = line.substring(3).trim().toLowerCase().split("\\s+");
                 Parser parse = new Parser(params);
-                
-                switch (code)
-                {
+
+                switch (code) {
                     case "cam":
                         // px   	py   	pz 	lx  	ly  	lz 	ux  	uy  	uz 	sc_dist	sc_width
                         camera = new Camera(parse.aVec3(), parse.aVec3(), parse.aVec3(),
@@ -133,7 +116,8 @@ public class RayTracer
                         scene.backgroundColor = parse.aColor();
                         scene.shadowRayCount = parse.aInt();
                         scene.maximumRecursionCount = parse.aInt();
-                        scene.superSamplingLevel = parse.aInt();
+                        if (parse.notDone())
+                            scene.superSamplingLevel = parse.aInt();
                         System.out.println(String.format("Parsed general settings (line %d)", lineNum));
                         break;
                     case "mtl":
@@ -174,48 +158,42 @@ public class RayTracer
                         break;
                 }
             }
-            
+
             // It is recommended that you check here that the scene is valid,
             // for example camera settings and all necessary materials were defined.
-            
+
             System.out.println("Finished parsing scene file " + sceneFileName);
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Failed to find file in working directory: " + System.getProperty("user.dir"));
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
     }
-    
+
     /**
      * Renders the loaded scene and saves it to the specified file location.
      */
-    private void renderScene(String outputFileName)
-    {
+    private void renderScene(String outputFileName) {
         long startTime = System.currentTimeMillis();
-        
+
         // Create a byte array to hold the pixel data:
         byte[] rgbData = raycastScene(camera, scene, imageWidth, imageHeight);
         long endTime = System.currentTimeMillis();
         Long renderTime = endTime - startTime;
-        
+
         // The time is measured for your own conveniece, rendering speed will not affect your score
         // unless it is exceptionally slow (more than a couple of minutes)
         System.out.println("Finished rendering scene in " + renderTime.toString() + " milliseconds.");
-        
+
         // This is already implemented, and should work without adding any code.
         saveImage(imageWidth, rgbData, outputFileName);
-        
+
         System.out.println("Saved file " + outputFileName);
     }
-    
-    private byte[] raycastScene(Camera camera, Scene scene, int pixelWidth, int pixelHeight)
-    {
+
+    private byte[] raycastScene(Camera camera, Scene scene, int pixelWidth, int pixelHeight) {
         double screenHeight = camera.screenWidth / pixelWidth * pixelHeight;
         byte[] rgbData = new byte[imageWidth * imageHeight * 3];
         Vec3 onePixelTowardsRight = camera.right.scaledBy(camera.screenWidth / pixelWidth);
@@ -224,49 +202,43 @@ public class RayTracer
                 .plus(camera.forward.scaledBy(camera.screenDistance)) // forward by camera distance
                 .plus(camera.right.scaledBy(-camera.screenWidth / 2)) // left by half screen
                 .plus(camera.up.scaledBy(-screenHeight / 2)); // down by half screen
-        for (int y = 0; y < pixelHeight; y++)
-        {
-            for (int x = 0; x < pixelWidth; x++)
-            {
+        for (int y = 0; y < pixelHeight; y++) {
+            for (int x = 0; x < pixelWidth; x++) {
                 Vec3 rayDirection = (currentScreenPoint.minus(camera.position)).normalized();
                 Intersection intersection = scene.raycast(camera.position, rayDirection);
                 Color color = scene.getColor(intersection);
-                int pixelIndex = (x * imageWidth + y) * 3;
+                int pixelIndex = ((pixelHeight - y - 1) * imageHeight + x) * 3;
                 rgbData[pixelIndex] = color.getRed();
                 rgbData[pixelIndex + 1] = color.getGreen();
                 rgbData[pixelIndex + 2] = color.getBlue();
-            
+
                 currentScreenPoint = currentScreenPoint.plus(onePixelTowardsRight); // →
             }
             currentScreenPoint = currentScreenPoint.minus(onePixelTowardsRight.scaledBy(pixelWidth)); // ← ← ← ← ←
             currentScreenPoint = currentScreenPoint.plus(onePixelTowardsUp); // ↑
         }
+
         return rgbData;
     }
-    
+
     //////////////////////// FUNCTIONS TO SAVE IMAGES IN PNG FORMAT //////////////////////////////////////////
-    
+
     /*
      * Saves RGB data as an image in png format to the specified location.
      */
-    private static void saveImage(int width, byte[] rgbData, String fileName)
-    {
-        try
-        {
+    private static void saveImage(int width, byte[] rgbData, String fileName) {
+        try {
             BufferedImage image = bytes2RGB(width, rgbData);
             ImageIO.write(image, "png", new File(fileName));
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("ERROR SAVING FILE: " + e.getMessage());
         }
     }
-    
+
     /*
      * Producing a BufferedImage that can be saved as png from a byte array of RGB values.
      */
-    private static BufferedImage bytes2RGB(int width, byte[] buffer)
-    {
+    private static BufferedImage bytes2RGB(int width, byte[] buffer) {
         int height = buffer.length / width / 3;
         ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_sRGB);
         ColorModel cm = new ComponentColorModel(cs, false, false,
@@ -275,14 +247,12 @@ public class RayTracer
         DataBufferByte db = new DataBufferByte(buffer, width * height);
         WritableRaster raster = Raster.createWritableRaster(sm, db, null);
         BufferedImage result = new BufferedImage(cm, raster, false, null);
-        
+
         return result;
     }
-    
-    public static class RayTracerException extends Exception
-    {
-        RayTracerException(String msg)
-        {
+
+    public static class RayTracerException extends Exception {
+        RayTracerException(String msg) {
             super(msg);
         }
     }
