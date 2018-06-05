94232e7cd1f8d7ae1df0661745ac33f3a8cc3dfa
itamar <itamar@feezback.com>
2018-06-05 13:29:38 +0300

    Added triangle intersection and it works!

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,11 +1,16 @@
 public class Triangle extends Shape
 {
     final Vec3[] vertices; // size = 3
+    //extras
+    final Plane plane; // is defined from vertices. Material is the same.
     
     public Triangle(Vec3 vertex1, Vec3 vertex2, Vec3 vertex3, int materialIndex)
     {
         super(materialIndex);
         this.vertices = new Vec3[]{vertex1, vertex2, vertex3};
+        Vec3 normal = vertex1.minus(vertex2).cross(vertex2.minus(vertex3)).normalized();
+        double offset = vertex1.projectedOn(normal).magnitude();
+        this.plane = new Plane(normal, offset, materialIndex);
     }
     
     public String toString()
@@ -16,8 +21,21 @@ public class Triangle extends Shape
     @Override
     public Intersection findRayIntersection(Vec3 origin, Vec3 direction)
     {
-        // first check is in plane of triangle
-        // check point is inside the triangle
-        return null;//TODO
+        Intersection planeIntersection = plane.findRayIntersection(origin, direction);
+        if (planeIntersection == null)
+            return null;
+        Vec3 point = planeIntersection.position;
+        Vec3 lineToPoint = point.minus(origin);
+        // check that the point is inside the triangle
+        for (int side = 0; side < 3; side ++) {
+            Vec3 V1 = vertices[side].minus(point);
+            Vec3 V2 = vertices[(side + 1) % 3].minus(point);
+            Vec3 normalToSide = V2.cross(V1).normalized();
+            double projOnNormToSide = lineToPoint.dot(normalToSide);
+            if (projOnNormToSide < 0)
+                return null;
+        }
+
+        return planeIntersection;
     }
 }
